3a0cb0b033f87d6552b370079e88e2a5f082b123
Aldwin Lubrica <aldwin.lub@gmail.com>
2021-06-16 23:03:19 -0700

    building the basic folder structure

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models')
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                },
+                {
+                    model: Comment,
+                    attributes: ['content', 'date_created', 'user_id']
+                }
+            ]
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', 
+            {
+                posts,
+                loggedIn: req.session.logged_in
+            });
+    } catch (err) {
+        res.status(500).json(err);
+     }
+});
+
+router.get('/post/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+          include: [
+            {
+                model: User,
+                attributes: ['name']
+            },
+            {
+                model: Comment,
+                attributes: ['content', 'date_created', 'user_id']
+            }
+          ]
+        });
+    
+        const post = postData.get({ plain: true });
+    
+        res.render('post', {
+          ...post,
+          loggedIn: true
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+});
+
+// add a post
+router.get('/addpost', (req, res) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+        return;    
+    };
+    res.render('addpost');
+});
+
+// be able to log in
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    };
+    res.render('/login');
+});
+
+module.exports = router;
+    
